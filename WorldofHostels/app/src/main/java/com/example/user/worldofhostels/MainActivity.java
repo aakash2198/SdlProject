4bd3c1ac4676ab8a203522c6737b243862ad609d
aakash2198 <aakashchavan1998@gmail.com>
2018-09-08 15:09:00 +0530

    First Update

---
+++ WorldofHostels/app/src/main/java/com/example/user/worldofhostels/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.user.worldofhostels;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    SeekBar seekBar,seekBar1;
+    TextView textView,textView1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekBar = (SeekBar) findViewById(R.id.distnace);
+        textView = (TextView) findViewById(R.id.distance_show);
+        seekBar1 = (SeekBar) findViewById(R.id.budget);
+        textView1 = (TextView) findViewById(R.id.budget_show);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textView.setText(i+" ");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                textView1.setText((i+3)*1000+" ");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+    }
+}
